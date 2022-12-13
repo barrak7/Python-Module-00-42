e6e0f07aa4b4ff494b75d92b1f054e944e099dc1
Hamza Barrak <hamzabarrak83@gmail.com>
2022-12-13 09:33:01 -0500

    Success

--- ex10/loading.py
+++ ex10/loading.py
@@ -4,13 +4,19 @@ from time import sleep
 def ft_progress(lst):
     s = time.time()
     i = s
+    l = len(lst)
+    prev_len = 0
     for e in lst:
         ela = time.time() - s
-        i = (time.time() - i) * (len(lst) - (lst.index(e) + 1))
-        per = (e + 1) / len(lst) * 100
-        pr = ("\rETA: {}s [ {}%][{}] {}/{} | elapsed time {}s".format(round(i, 2), round(per, 2), "="*(int(per / 10))+">"+" "*(int((100 - per) / 10)), e + 1, len(lst), round(ela, 2)))
-        print(pr, end="")
+        i = (time.time() - i) * (l - (lst.index(e) + 1))
+        per = (e + 1) / l * 100
+        pr = ("ETA: {}s [ {}%][{}] {}/{} | elapsed time {}s".format(round(i, 2), int(per), "="*(int(per / 10))+">"+" "*(int((100 - per) / 10)), e + 1, l, round(ela, 2)))
+        lp = len(pr)
+        if (prev_len > lp):
+            prev_len = prev_len - lp
+            print(pr+" "*prev_len, end="\r")
         i = time.time()
+        prev_len = lp
         yield e
 
 listy = range(1000)
